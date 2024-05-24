e5af2cd80ce35c85c6d2ac6c75772b0254a9e140
Mateusz1361 <ml307892@student.polsl.pl>
2024-05-24 23:00:07 +0200

    confusion matrix has been added

--- analyse.py
+++ analyse.py
@@ -1,7 +1,6 @@
 import pickle
 import random as rd
 
-
 def loadDataset(filename):
     dataset = []
     with open(filename, 'rb') as f:
@@ -13,10 +12,10 @@ def loadDataset(filename):
                 break
     return dataset
 
-
 def splitDataset(dataset, split, trset, teset):
-    rd.seed(1)
+    rd.seed(15)
     rd.shuffle(dataset)
+
     splitRange = int(split * len(dataset))
     for i in range(splitRange):
         trset.append(dataset[i])
@@ -26,12 +25,56 @@ def splitDataset(dataset, split, trset, teset):
 
 def getAccuracy(testSet, prediction):
     correct = 0
-    genreLabels = {1: 'classical', 2: 'disco', 3: 'hiphop', 4: 'metal'}
+    genreLabels = {0: "classical", 1: "disco", 2: "hiphop", 3: "metal", 4: "blues", 5: "country"}
+    genres = list(genreLabels.values())
+    confusionMatrix=[[0 for j in range(6)]for i in range(6)]
+
+    TP = {genre: 0 for genre in genres}     # True Positive
+    TN = {genre: 0 for genre in genres}     # True Negative
+    FP = {genre: 0 for genre in genres}     # False Positive
+    FN = {genre: 0 for genre in genres}     # False Negative
 
     for x in range(len(testSet)):
-        genreName = genreLabels.get(testSet[x][-1])
-        predictionName = genreLabels.get(prediction[x])
-        print(f"To jest : {genreName}, a to nasze przewidywanie {predictionName}")
+        true_label = genreLabels.get(testSet[x][-1])
+        predicted_label = genreLabels.get(prediction[x])
+        confusionMatrix[prediction[x]][testSet[x][-1]] += 1
+
+        for genre in genres:
+            if true_label == genre:
+                if predicted_label == genre:
+                    TP[genre] += 1
+                else:
+                    FN[genre] += 1
+                    FP[predicted_label] += 1
+            elif predicted_label != genre:
+                TN[genre] += 1
+
+        print(f"To jest : {true_label}, a to nasze przewidywanie {predicted_label}")
         if testSet[x][-1] == prediction[x]:
             correct += 1
-    return 1.0 * correct / len(testSet)
+    for i in range(6):
+        print(confusionMatrix[i])
+
+    metrics = {}
+    for genre in genres:
+        accuracy = (TP[genre] + TN[genre]) / (TP[genre] + TN[genre] + FP[genre] + FN[genre])
+        sensitivity = TP[genre] / (TP[genre] + FN[genre])
+        precision = TP[genre] / (TP[genre] + FP[genre])
+        F1 = 2 * (precision * sensitivity) / (precision + sensitivity)
+        specificity = TN[genre] / (TN[genre] + FP[genre])
+
+        metrics[genre] = {
+            'Accuracy': round(accuracy * 100, 2),
+            'Sensitivity-Recall': round(sensitivity * 100, 2),
+            'Precision': round(precision * 100, 2),
+            'F1': round(F1 * 100, 2),
+            'Specificity': round(specificity * 100, 2)
+        }
+
+    for genre, metric in metrics.items():
+        print(f"{genre.capitalize()} metrics:")
+        for key, value in metric.items():
+            print(f"{key}: {value}%")
+        print("\n")
+
+    return correct / len(testSet)
--- mfcc.py
+++ mfcc.py
@@ -1,52 +1,51 @@
+import time
 import numpy as np
 import scipy.fftpack as fftp
 
-def freq_to_mel[T](f: T) -> T:
+def freq_to_mel(f, /):
     return 1125.0 * np.log(1.0 + f / 700.0)
 
-def mel_to_freq[T](m: T) -> T:
+def mel_to_freq(m, /):
     return 700.0 * (np.exp(m / 1125.0) - 1.0)
 
-def highpass_filter(audio: np.ndarray) -> np.ndarray:
-    newAudio = np.empty(len(audio),dtype = float)
-    newAudio[0] = audio[0]
-    for i in range(1,len(audio)):
-        newAudio[i] = audio[i] - 0.97 * audio[i - 1]
-    return newAudio
+def highpass_filter_in_place(audio: np.ndarray, /):
+    audio -= 0.97 * np.append(audio[1:], [0.0])
 
-def compute_filterbanks(fftLen: int,numBanks: int,sampleRate: int,lowFreq: int,highFreq: int) -> np.ndarray:
-    mels = np.linspace(freq_to_mel(lowFreq),freq_to_mel(highFreq),numBanks + 2)
+def compute_filterbanks(fftLen: int, numBanks: int, sampleRate: int, lowFreq: int, highFreq: int) -> np.ndarray:
+    mels = np.linspace(freq_to_mel(lowFreq), freq_to_mel(highFreq), numBanks + 2)
     bins = np.floor((fftLen + 1) * mel_to_freq(mels) / sampleRate)
 
-    filterbanks = np.zeros((numBanks,fftLen // 2 + 1),dtype = float)
+    filterbanks = np.zeros((numBanks, fftLen // 2 + 1), dtype=float)
     for bankIndex in range(numBanks):
-        for index in range(int(bins[bankIndex]),int(bins[bankIndex + 1])):
-            filterbanks[bankIndex,index] = (index - bins[bankIndex]) / (bins[bankIndex + 1] - bins[bankIndex])
-        for index in range(int(bins[bankIndex + 1]),int(bins[bankIndex + 2])):
-            filterbanks[bankIndex,index] = (bins[bankIndex + 2] - index) / (bins[bankIndex + 2] - bins[bankIndex + 1])
+        for index in range(int(bins[bankIndex]), int(bins[bankIndex + 1])):
+            filterbanks[bankIndex, index] = (index - bins[bankIndex]) / (bins[bankIndex + 1] - bins[bankIndex])
+        for index in range(int(bins[bankIndex + 1]), int(bins[bankIndex + 2])):
+            filterbanks[bankIndex, index] = (bins[bankIndex + 2] - index) / (bins[bankIndex + 2] - bins[bankIndex + 1])
     return filterbanks
 
-def mfcc(audio: np.ndarray,sampleRate: int,numFilterBanks: int = 26,numCepCoeffs: int = 13,fftLen: int = 512,winLen: float = 0.025,winStep: float = 0.01) -> np.ndarray:
-    audio = highpass_filter(audio)
+def mfcc(audio: np.ndarray, sampleRate: int, /, *, numFilterBanks: int = 26, numCepCoeffs: int = 13, fftLen: int = 512,
+         winLen: float = 0.025, winStep: float = 0.01) -> np.ndarray:
+    audio = audio.astype(float)
+    highpass_filter_in_place(audio)
 
     winLenRate = int(winLen * sampleRate)
     winStepRate = int(winStep * sampleRate)
 
-    banks = compute_filterbanks(fftLen,numFilterBanks,sampleRate,0,sampleRate // 2)
-    result = np.zeros(((len(audio) + winStepRate - len(audio) % winStepRate) // winStepRate,numCepCoeffs))
+    banks = compute_filterbanks(fftLen, numFilterBanks, sampleRate, 0, sampleRate // 2)
+    result = np.zeros(((len(audio) + winStepRate - len(audio) % winStepRate) // winStepRate, numCepCoeffs))
 
     resultI = 0
-    for i in range(0,len(audio),winStepRate):
-        frame = audio[i:min(i + winLenRate,len(audio))]
+    for i in range(0, len(audio), winStepRate):
+        frame = audio[i:min(i + winLenRate, len(audio))]
         if len(frame) < winLenRate:
-            frame = np.append(frame,np.zeros(winLenRate - len(frame)))
+            frame = np.append(frame, np.zeros(winLenRate - len(frame)))
 
-        powerSpectrum = np.square(np.abs(np.fft.rfft(frame,fftLen))) / winLenRate
+        powerSpectrum = np.square(np.abs(np.fft.rfft(frame, fftLen))) / winLenRate
 
-        features = np.dot(powerSpectrum,banks.T)
-        features = np.where(features == 0,np.finfo(float).eps,features)
+        features = np.dot(powerSpectrum, banks.T)
+        features = np.where(features == 0, np.finfo(float).eps, features)
 
-        result[resultI] = fftp.dct(np.log(features),type = 2,norm = "ortho")[:numCepCoeffs]
+        result[resultI] = fftp.dct(np.log(features), type=2, norm="ortho")[:numCepCoeffs]
         resultI += 1
     return result
 
@@ -56,35 +55,21 @@ def main():
     import traceback
     import scipy.io.wavfile as wav
 
-    directory = "./data"
-    with open("./my.dat","wb") as f:
-        i = 0
-        for folder in os.listdir(directory):
-            i += 1
-            path = directory + "/" + folder
+    startTime = time.perf_counter_ns()
+    Directories = ["classical", "disco", "hiphop", "metal", "blues", "country"]
+    with open("./my.dat", "wb") as f:
+        for i in range(len(Directories)):
+            path = "./data/" + Directories[i]
             for file in os.listdir(path):
                 try:
-                    (rate,sig) = wav.read(path + "/" + file)
-                    mfcc_feat = mfcc(sig,rate)
+                    (rate, sig) = wav.read(path + "/" + file)
+                    mfcc_feat = mfcc(sig, rate)
                     covariance = np.cov(np.matrix.transpose(mfcc_feat))
-                    pickle.dump((mfcc_feat.mean(0),covariance,i),f)
+                    pickle.dump((covariance, i), f)
                 except Exception as e:
-                    print("Got an exception:",traceback.format_exc(),'in folder:',folder,'filename:',file)
-
-    #import scipy.io.wavfile as wav
-    #(freq,audio) = wav.read("./data/classical/classical.00000.wav")
-    #coeffs = mfcc(audio,freq)
-    #print(coeffs)
-
-    #audio = audio / max(audio)
-    #newAudio = mfcc_emphasize(audio)
-    #(figure,plots) = plot.subplots(1,2)
-    #figure.suptitle("Audio")
-    #plots[0].plot(audio)
-    #plots[0].set_ylim([-1,1])
-    #plots[1].plot(newAudio)
-    #plots[1].set_ylim([-1,1])
-    #plot.show()
+                    print(f"Wyjątek w folderze '{Directories[i]}' w pliku '{file}': {traceback.format_exc()}")
+    endTime = time.perf_counter_ns()
+    print(f"Czas wykonania: {(endTime - startTime) / 1e9} s")
 
 if __name__ == "__main__":
     main()
